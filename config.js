f2221d66f25e18ae285ee0e92d0266684afc0e88
Lior Edri <liorbitan12@gmail.com>
2024-10-01 13:46:42 +0300

    url

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-3309b086-930b-4c63-a3b6-860acaa80a2e.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-343c112d-8280-4223-9dc3-5d337babf3cf.containerhub.tripleten-services.com"
 }
