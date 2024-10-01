2c307892612b15833834019893a2c34d5de4deb3
Lior Edri <liorbitan12@gmail.com>
2024-10-01 11:33:50 +0300

    put request

--- tests/putHandlers.test.js
+++ tests/putHandlers.test.js
@@ -5,7 +5,7 @@ const requestBody = {}
 
 test('', async () => {
     try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`, {
+		const response = await fetch(`${config.API_URL}/api/v1/products/7`, {
 			method: 'PUT',
 			headers: {
 			'Content-Type': 'application/json'
