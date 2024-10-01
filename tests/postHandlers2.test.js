52eb4565c288fd9891cc6b54a467aba8b1cde3b8
Lior Edri <liorbitan12@gmail.com>
2024-10-01 10:47:34 +0300

    second post test

---
+++ tests/postHandlers2.test.js
@@ -0,0 +1,37 @@
+// eslint-disable-next-line no-undef
+const config = require('../config');
+
+const requestBody = {
+        "products": [
+        {
+            "id": 1,
+            "quantity": 3
+        },
+        {
+            "id": 4,
+            "quantity": 1
+        },
+        {
+            "id": 9,
+            "quantity": 3
+        }
+    ],
+    "deliveryTime": 7
+}
+
+test('', async () => {
+	let response
+    try {
+			response = await fetch(`${config.API_URL}/api/v1/couriers/check`, {
+			method: 'POST',
+			headers: {
+			'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(requestBody)
+		});
+	} catch (error) {
+		console.error(error);
+	}
+	const costCheck = await response.json();
+	expect (costCheck['Speedy']['deliveryPrice']).toBe(7);
+});
