aa724644b27d2d50d43d43e98ba839ebeb8dbffc
Lior Edri <liorbitan12@gmail.com>
2024-10-01 11:03:10 +0300

    status 404

---
+++ tests/getHandlers2.test.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-undef
+const config = require('../config');
+
+test('', async () => {
+	let actualStatus
+	try {
+		const response = await fetch(`${config.API_URL}/api/v1/couriers`);
+		actualStatus = response.status;
+	} catch (error) {
+		console.error(error);
+	}
+	expect(actualStatus).toBe(404);
+});
