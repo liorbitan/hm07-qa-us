12da501e536869fd4cc4ca74dddf26d2abc37985
Lior Edri <liorbitan12@gmail.com>
2024-10-08 12:48:58 +0300

    put 1 changes

--- tests/putHandlers.test.js
+++ tests/putHandlers.test.js
@@ -3,7 +3,7 @@ const config = require('../config');
 
 const requestBody = {}
 
-test('', async () => {
+test('Check the response body should be 175', async () => {
 	let response
     try {
 		const response = await fetch(`${config.API_URL}/api/v1/products/7`, {
@@ -13,7 +13,7 @@ test('', async () => {
 			},
 			body: JSON.stringify(requestBody)
 		});
-	const requestBody = await response.json();
+	const requestBody = await response.json(175);
 	} catch (error) {
 		console.error(error);
 	}
